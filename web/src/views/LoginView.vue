57603eb3a38d5cd6509f60345c613fbe6e0b0c1e
Sandovl0593 <adrian.sandoval@utec.edu.pe>
2023-05-08 12:07:47 -0500

    config to exec MV

--- web/src/views/LoginView.vue
+++ web/src/views/LoginView.vue
@@ -1,56 +1,56 @@
 <template>
-    <div>
-      <label>Usuario</label>
-      <input type="text" v-bind:value="nombre_usuario" v-on:input="verificar_nombre"><br>
-      <br>
-      <label>Contraseña</label>
-      <input type="text" v-bind:value="contrasenha" v-on:input="verificar_contrasenha"><br>
-      <br>
-      <button v-on:click="casa">Login</button>
-    </div>
-    <nav>
-      <router-link to="/registrar">Registrarse</router-link>
-    </nav>
-  </template>
-  
-  <script>
-  export default {
-    name: "LoginView",
-    data(){
-      return{
-        nombre_usuario: "",
-        contrasenha: ""
-      }
+  <div>
+    <label>Usuario</label>
+    <input type="text" v-bind:value="nombre_usuario" v-on:input="verificar_nombre"><br>
+    <br>
+    <label>Contraseña</label>
+    <input type="text" v-bind:value="contrasenha" v-on:input="verificar_contrasenha"><br>
+    <br>
+    <button v-on:click="casa">Login</button>
+  </div>
+  <nav>
+    <router-link to="/registrar">Registrarse</router-link>
+  </nav>
+</template>
+
+<script>
+export default {
+  name: "LoginView",
+  data(){
+    return{
+      nombre_usuario: "",
+      contrasenha: ""
+    }
+  },
+  methods: {
+    casa(){
+      this.$store.dispatch("accion_act_usuario", this.nombre_usuario)
+      this.$store.dispatch("accion_act_contra", this.contrasenha)
+      this.$router.push('/home')
     },
-    methods: {
-      casa(){
-        this.$store.dispatch("accion_act_usuario", this.nombre_usuario)
-        this.$store.dispatch("accion_act_contra", this.contrasenha)
-        this.$router.push('/home')
-      },
-      verificar_nombre(e){
-        this.nombre_usuario = e.target.value
-      },
-      verificar_contrasenha(e){
-        this.contrasenha = e.target.value
-      },
-      async verificar_usuario(){
-        let v_usuario = {nombre_usuario: this.nombre_usuario, contrasenha: this.contrasenha}
-        await fetch('http://127.0.0.1:5000/utecshop/login', {
-          method: 'POST',
-          headers: {
-            'Content-type': 'application/json'
-          },
-          body: JSON.stringify(v_usuario)
-        }).then((resp)=> resp.json()).then((datos)=>{
-          if(datos.nombre_usuario === this.nombre_usuario && datos.contrasenha === this.contrasenha)
-            this.casa()
-        })
-      }
+    verificar_nombre(e){
+      this.nombre_usuario = e.target.value
+    },
+    verificar_contrasenha(e){
+      this.contrasenha = e.target.value
+    },
+    async verificar_usuario(){
+      let v_usuario = {nombre_usuario: this.nombre_usuario, contrasenha: this.contrasenha}
+      await fetch('http://127.0.0.1:5000/utecshop/login', {
+        method: 'POST',
+        headers: {
+          'Content-type': 'application/json'
+        },
+        body: JSON.stringify(v_usuario)
+      }).then((resp)=> resp.json()).then((datos)=>{
+        if(datos.nombre_usuario === this.nombre_usuario && datos.contrasenha === this.contrasenha)
+          this.casa()
+      })
     }
   }
-  </script>
+}
+</script>
   
-  <style scoped>
-  
-  </style>
+<style scoped>
+
+</style>
