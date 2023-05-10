3fd93ca9c3196fc8c6845771ccc0c79873c3c4e7
Sandovl0593 <adrian.sandoval@utec.edu.pe>
2023-05-10 15:17:34 -0500

    fix 2

--- microservice1/app/routes.py
+++ microservice1/app/routes.py
@@ -3,6 +3,6 @@ from app import api
 
 # API resource routes
 api.add_resource(Index, '/')
-api.add_resource(UserList, '/utecshop/users', endpoint='users')
-api.add_resource(User, '/utecshop/user/<username>', endpoint='user')
+api.add_resource(UserList, '/utecshop/usuarios', endpoint='usuarios')
+api.add_resource(User, '/utecshop/usuario/<username>', endpoint='usuario')
 api.add_resource(Register, '/utecshop/register', endpoint='register')
