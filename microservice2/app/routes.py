1ea2d81dd00176f9b5018ceff5334a30c294df55
Sandovl0593 <adrian.sandoval@utec.edu.pe>
2023-05-10 15:47:26 -0500

    fix 3

--- microservice2/app/routes.py
+++ microservice2/app/routes.py
@@ -1,9 +1,7 @@
-from controllers import *
-from app import api
 from app.controllers import *
 from app import api
 
 # API resource routes
 api.add_resource(RegistrarProducto, '/utecshop/registar_producto', endpoint='registrar_producto')
 api.add_resource(Comprar, '/utecshop/comprar', endpoint='comprar')
-api.add_resource(RegistrarCompr, '/utecshop/register', endpoint='register')
+api.add_resource(RegistrarCompra, '/utecshop/registrar_compra', endpoint='registrar_compra')
--- microservice3/app/routes.py
+++ microservice3/app/routes.py
@@ -1,2 +1,6 @@
-from controllers import *
+from app.controllers import *
 from app import api
+
+# API resource routes
+api.add_resource(Vender, '/utecshop/vender', endpoint='vender')
+api.add_resource(Inventario, '/utecshop/inventario', endpoint='inventario')
