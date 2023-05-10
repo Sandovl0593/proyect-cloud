279c10a34003b1618b4751e6af2206c2b34e6dd3
CRISTOPHER2207 <cristopher.meneses@utec.edu.pe>
2023-05-10 15:38:47 -0500

    microservicio2final

--- microservice2/app/controllers.py
+++ microservice2/app/controllers.py
@@ -3,31 +3,29 @@ from flask import jsonify, request
 from flask_restful import Resource
 
 
-class Vender(Resource):
+class RegistrarCompra(Resource):
     def post(self):
         try:
             conn = db.connect()
             cursor = conn.cursor()
 
-            producto_u = request.get_json()
-            usuario_l = producto_u['usuario']
-            query = "SELECT * FROM proyecto.producto WHERE usuario_p = %s"
-            cursor.execute(query, (usuario_l,))
-            productos_l = cursor.fetchall()
+            nueva_compra = request.get_json()
+            codigo_p = nueva_compra['codigo_producto']
+            comprador = nueva_compra['usuario_comprador']
+            vendedor = nueva_compra['usuario_vendedor']
 
-            productos_d = []
-            for i in productos_l:
-                producto_d = {"codigo": i[0], "nombre": i[1],
-                              "precio": i[2], "marca": i[3], "tipo": i[4]}
-                productos_d.append(producto_d)
+            query = """INSERT INTO proyecto.compra(codigo_p, usuario_c, usuario_v)
+                       VALUES(%s, %s, %s)"""
 
-            response = jsonify(productos_d)
+            cursor.execute(query, (codigo_p, comprador, vendedor))
+            conn.commit()
+
+            response = jsonify(message='Compra agregada exitosamente.', id=cursor.lastrowid)
             response.status_code = 200
 
-        except Exception as e:
-            response = jsonify(message='Failed to get productos.')
+        except Exception:
+            response = jsonify(message='No se pudo agregar la compra.')
             response.status_code = 400
-            print(e)
 
         finally:
             cursor.close()
--- microservice3/app/controllers.py
+++ microservice3/app/controllers.py
@@ -2,3 +2,35 @@ from app import app, db
 from flask import jsonify, request
 from flask_restful import Resource
 
+class Vender(Resource):
+    def post(self):
+        try:
+            conn = db.connect()
+            cursor = conn.cursor()
+
+            producto_u = request.get_json()
+            usuario_l = producto_u['usuario']
+            query = "SELECT * FROM proyecto.producto WHERE usuario_p = %s"
+            cursor.execute(query, (usuario_l,))
+            productos_l = cursor.fetchall()
+
+            productos_d = []
+            for i in productos_l:
+                producto_d = {"codigo": i[0], "nombre": i[1],
+                              "precio": i[2], "marca": i[3], "tipo": i[4]}
+                productos_d.append(producto_d)
+
+            response = jsonify(productos_d)
+            response.status_code = 200
+
+        except Exception as e:
+            response = jsonify(message='Failed to get productos.')
+            response.status_code = 400
+            print(e)
+
+        finally:
+            cursor.close()
+            conn.close()
+            return response
+
+
