e637da7b5a42ce2936e9e5e18a209d5e4797fc66
CRISTOPHER2207 <cristopher.meneses@utec.edu.pe>
2023-05-10 15:49:23 -0500

    microservicio2final

--- microservice3/app/controllers.py
+++ microservice3/app/controllers.py
@@ -32,5 +32,38 @@ class Vender(Resource):
             cursor.close()
             conn.close()
             return response
+        
+class Inventario(Resource):
+    def post(self):
+        try:
+            conn = db.connect()
+            cursor = conn.cursor()
 
+            compra_u = request.get_json()
+            usuario_l = compra_u['usuario']
 
+            query = """SELECT codigo_c, codigo_p, usuario_c, usuario_v
+                       FROM proyecto.compra
+                       WHERE usuario_c = %s"""
+
+            cursor.execute(query, (usuario_l,))
+            compras_l = cursor.fetchall()
+
+            compras_d = []
+            for i in compras_l:
+                compra_d = {"codigo_c": i[0], "codigo_p": i[1],
+                            "usuario_c": i[2], "usuario_v": i[3]}
+                compras_d.append(compra_d)
+
+            response = jsonify(compras_d)
+            response.status_code = 200
+
+        except Exception as e:
+            response = jsonify(message='No se pudo obtener el inventario.')
+            response.status_code = 400
+            print(e)
+
+        finally:
+            cursor.close()
+            conn.close()
+            return response
