7351782b0129a89ed5b05f6789431caabba413ef
Sandovl0593 <adrian.sandoval@utec.edu.pe>
2023-05-12 21:28:51 -0500

    fix

--- container-stop.sh
+++ container-stop.sh
@@ -7,5 +7,5 @@ do
   # Verifica si la imagen existe en el sistema
   if docker images --format '{{.Repository}}:{{.Tag}}' | grep -q "$imagen"; then
     imagen_id=$(docker inspect -f '{{.Image}}' $imagen 2>/dev/null)
-    docker rmi $imagen_id --force
+    docker stop $imagen_id
 done
